da2d8d97ba02d7707fa976e5ed9264f2ad7c59ef
Meabhijit11 <meabhijitmeshram@gmail.com>
2023-11-19 10:35:27 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from src.cnnClassifier import logger
-from src.cnnClassifier.pipeline.stage01_data_injestion import DataIngestionTrainingPipeline
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage01_data_injestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"      # define the stage name
 
@@ -16,4 +17,17 @@ if __name__ == '__main__':
 # this we have copied from Pipeline
 
 
+STAGE_NAME = "Prepare base model"
 
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+# this we have copied from Pipeline
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,7 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
 from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
 
 class ConfigurationManager:
     def __init__(
@@ -27,4 +28,22 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:    # This function will return the value which is defined in the Class "PrepareBaseModelConfig"
+        config = self.config.prepare_base_model                          # using "prepare_base_model" we are extracting the things which is given by "prepare_base_model" as per "config.yaml" file
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
 
